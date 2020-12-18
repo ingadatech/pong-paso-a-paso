cc6b5b0c1114a64d2848a3ad3967208e961e9dbb
Diego Mariño <diegomarino@users.noreply.github.com>
2020-12-18 11:52:01 +0100

    Funciones pricipales creadas (iniciaPartida, iniciaRonda, defineVariables...)

--- main.ts
+++ main.ts
@@ -1,1 +1,25 @@
-
+function finalizaRonda () {
+	
+}
+function iniciaRonda () {
+	
+}
+function finalizaPartida () {
+	
+}
+function defineVariables () {
+    maximaPuntuacion = 5
+}
+function iniciaPartida () {
+    defineVariables()
+    creaObjetos()
+    iniciaRonda()
+}
+function creaObjetos () {
+	
+}
+function posicionaObjetos () {
+	
+}
+let maximaPuntuacion = 0
+iniciaPartida()
